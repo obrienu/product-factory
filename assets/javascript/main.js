9297045c218dc83a72ab961c89cd4e12b4a1e1b5
obrienu <obrienules@gmail.com>
2020-07-16 21:25:41 +0100

    Complete landing page

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,21 @@
 const menuButton = document.querySelector("#breadcrumb");
 const header = document.querySelector(".header");
-menuButton.addEventListener("click", toggleMenu)
+const expandAnswerButton = document.querySelectorAll(".expand_answer");
+
+
+menuButton.addEventListener("click", toggleMenu);
+
+expandAnswerButton.forEach(button => {
+    button.addEventListener("click", toggleAnswer)
+});
 
 function toggleMenu(event) {
     event.preventDefault();
     menuButton.classList.toggle("menu_open");
     header.classList.toggle("show_nav");
+}
+
+function toggleAnswer(event){
+    event.preventDefault();
+    this.parentNode.nextElementSibling.classList.toggle("open_answer")
 }
